60a5ae4f4500f6cae661ec7382948cf4679d92f7
Pedro Henrique Ramos Martins <65059202+ProgPedrao@users.noreply.github.com>
2023-10-03 19:14:29 -0300

    Finalizando o projeto Smartphone.
    Encerrando o desafio proposto na plataforma da DIO.
    
    estudando POO, abstração, Herança, encapsulamento e polimorfismo.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Tijolão", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "XR", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
